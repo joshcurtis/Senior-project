aca88dd6eaf15e69cceaa3e143f7ba28495edc84
Josh Curtis <jcurtis825@gmail.com>
2015-12-07 12:41:47 -0800

    Moved IniData into separate file

---
+++ src/ini_data.rs
@@ -0,0 +1,26 @@
+extern crate gtk;
+use gtk::traits::*;
+
+/**
+ * Contains shared data for manipulating INIs.
+ **/
+pub struct IniData<'a> {
+    pub section_values_vec: Vec<(&'a str, Vec<&'a str>)>,
+    pub entries_vec: Vec<Vec<gtk::Entry>>,
+    pub generate_button: gtk::Button,
+    pub open_button: gtk::Button
+}
+
+/**
+ * Currently, only used for IniData::new() which creates a default IniData object.
+ **/
+impl<'a> IniData<'a> {
+    pub fn new(initial_data: Vec<(&'a str, Vec<&'a str>)>) -> IniData<'a> {
+        IniData {
+            section_values_vec: initial_data,
+            entries_vec: Vec::new(),
+            generate_button: gtk::Button::new_with_label("Generate INI File").unwrap(),
+            open_button: gtk::Button::new_with_label("Load INI File").unwrap()
+        }
+    }
+}
