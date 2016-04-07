a176c283cee6dfc5f3404a200ead5a8e3b46ca6f
Josh Curtis <jcurtis825@gmail.com>
2016-04-07 11:16:00 -0700

    Created run-ssh-command

--- src/cljs/server_interop/core.cljs
+++ src/cljs/server_interop/core.cljs
@@ -60,23 +60,27 @@
   [callback]
   (remote-callback :connection-status [] callback))
 
+(defn- run-ssh-command
+  [command callback]
+  (remote-callback :run-ssh-command [command] callback))
+
 (defn watch-mk-services!
   "Watch mk"
   [callback]
-  (remote-callback :run-ssh-command
-                   ["sh ~/Desktop/resolve.sh"] callback))
+  (run-ssh-command "sh /home/machinekit/Desktop/resolve.sh" callback))
 
 (defn launch-mk!
   "Launch machinekit"
   [callback]
-  (remote-callback :run-ssh-command
-                   ["sh ~/Desktop/run.sh"] callback))
+  (run-ssh-command "sh /home/machinekit/Desktop/run.sh" callback))
 
 (defn cleanup!
-  "Convenience method for programming to cleanup processes that may be existing"
+  "
+  BROKEN killps probably isn't defined in the ssh environment
+  TODO: Fix
+  Convenience method for programming to cleanup processes that may be existing"
   [callback]
-  (remote-callback :run-ssh-command
-                   ["killps resolve.py && killps mklauncher"] callback))
+  (run-ssh-command "killps resolve.py && killps mklauncher" callback))
 
 (defn send-data
   "Function for quickly testing zmq functionality"
