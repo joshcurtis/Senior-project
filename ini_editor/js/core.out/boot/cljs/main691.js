ff77d4ef1c9562482b5aa6898cff9b9ae5e3e7fe
Josh Curtis <jcurtis825@gmail.com>
2016-04-16 21:56:06 -0700

    These commits didn't seem to do anything

--- ini_editor/js/core.out/boot/cljs/main691.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 1.7.170 {:static-fns true, :optimize-constants true}
-goog.provide('boot.cljs.main691');
-goog.require('cljs.core');
-goog.require('ini_editor.core');
-ini_editor.core.start();
