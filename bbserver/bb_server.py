5760f2550dd23ca09764edc2284597a5b8b83ac8
Shubham Gogna <s-gogna@users.noreply.github.com>
2016-04-25 07:27:41 -0700

    Create and use login route (#52)
    * Add login route to bb_server
    
    * Updated app to use login route
    
    * Update config route to use query params
    
    * Remove username from login
    - Username is returned as part of the login authentication (so it can be displayed in the remote tab)
    - Username is no longer neccesary for authentication and can be configured as part of bb_server

--- bbserver/bb_server.py
+++ bbserver/bb_server.py
@@ -8,6 +8,9 @@ import os.path
 app = Flask(__name__)
 app.debug = True
 
+login_username = 'machinekit'
+login_password = 'kit'
+
 # stolen code for crossdomain
 from datetime import timedelta
 from flask import make_response, request, current_app
@@ -70,6 +73,18 @@ def route_status():
     global status
     return edn.dumps(status)
 
+@app.route("/login", methods=['POST', 'OPTIONS'])
+@crossdomain(origin="*")
+def route_login():
+    global status
+    if request.method == 'OPTIONS':
+        return edn.dumps({})
+    if request.method == 'POST':
+        if request.form.get('password') == login_password:
+            return '{:authenticated true :username ' + login_username + '}'
+        else:
+            return '{:authenticated false}'
+
 @app.route("/configs", methods=['GET'])
 @crossdomain(origin="*")
 def route_configs():
@@ -82,11 +97,11 @@ def route_configs():
     m = dict(zip(m_keys, m_vals))
     return edn.dumps(m)
 
-@app.route("/configs/<config>/<filename>", methods=['GET', 'PUT', 'DELETE', 'OPTIONS'])
+@app.route("/config", methods=['GET', 'PUT', 'DELETE', 'OPTIONS'])
 @crossdomain(origin="*")
-def route_configs_file(config, filename):
+def route_configs_file():
     global config_root
-    path = "{}/{}/{}".format(config_root, config, filename)
+    path = "{}/{}".format(config_root, request.args.get('path'))
     if request.method == 'GET':
         txt = open(path).read()
         return edn.dumps({"contents": txt})
--- src/cljs/bbserver/core.cljs
+++ src/cljs/bbserver/core.cljs
@@ -11,22 +11,31 @@
   ([http-type address callback options]
    (go (let [options (merge {:with-credentials? false} options)
              resp (<! (http-type address
-                                 options))]
+                                 options))
+             {:keys [status body]} resp]
+         ; TODO Modify callbacks to take status + body
          (-> resp :body reader/read-string callback))))
   ([http-type address callback] (bb-wrapper http-type address callback {})))
 
 (defn- bb-get
-  [address callback]
-  (bb-wrapper http/get address callback))
+  ([address callback]
+  (bb-get address callback {}))
+  ([address callback options]
+  (bb-wrapper http/get address callback options)))
 
 (defn- bb-put
   [address callback options]
-  (js/alert (str options))
   (bb-wrapper http/put address callback options))
 
+(defn- bb-post
+  [address callback options]
+  (bb-wrapper http/post address callback options))
+
 (defn- bb-delete
-  [address callback]
-  (bb-wrapper http/delete address callback))
+  ([address callback]
+  (bb-delete address callback {}))
+  ([address callback options]
+  (bb-wrapper http/delete address callback options)))
 
 (defn- build-address
   [hostname port route]
@@ -36,6 +45,11 @@
   [hostname callback]
   (bb-get (build-address hostname 3001 "/status") callback))
 
+(defn login
+  [hostname password callback]
+  (bb-post (build-address hostname 3001 "/login") callback
+    {:form-params {:password password}}))
+
 (defn configs
   [hostname callback]
   (bb-get (build-address hostname 3001 "/configs") callback))
@@ -43,18 +57,21 @@
 (defn get-file
   [hostname config filename callback]
   {:pre [(string/includes? config \/)]}
-  (bb-get (build-address hostname 3001 (str "/configs/" config filename)) callback))
+  (bb-get (build-address hostname 3001 "/config") callback
+    {:query-params {:path (str config filename)}}))
 
 (defn put-file
   [hostname config filename contents callback]
   {:pre [(string/includes? config \/) (string? contents)]}
-  (bb-put (build-address hostname 3001 (str "/configs/" config filename))
-          callback {:form-params {:contents contents}}))
+  (bb-put (build-address hostname 3001 "/config") callback
+    {:query-params {:path (str config filename)}
+     :form-params {:contents contents}}))
 
 (defn delete-file
   [hostname config filename callback]
   {:pre [(string/includes? config \/)]}
-  (bb-delete (build-address hostname 3001 (str "/configs/" config filename)) callback))
+  (bb-delete (build-address hostname 3001 "/config") callback
+    {:query-params {:path (str config filename)}}))
 
 (defn get-services-log
   [hostname callback]
