b38484e5c14185a0045bc287b7b9a985e21b0a4c
Will <will.s.medrano@gmail.com>
2016-04-27 11:33:31 -0700

    Min Build (#62)
    Using Google Closure compiler

--- src/cljs/three/coordinates.cljs
+++ src/cljs/three/coordinates.cljs
@@ -2,6 +2,8 @@
   ""
   (:require [cljsjs.three]))
 
+(def line-segments (js/eval "THREE.LineSegments"))
+
 (defn build-axis
   [[start-x start-y start-z] [end-x end-y end-z] color dashed?]
   (let [geom (js/THREE.Geometry.)
@@ -12,8 +14,8 @@
                                                  :gapSize 3})
               (js/THREE.LineBasicMaterial. #js {:linewidth 3
                                                 :color color}))]
-    (.push (.-vertices geom) (js/THREE.Vector3. start-x start-y start-z))
-    (.push (.-vertices geom) (js/THREE.Vector3. end-x end-y end-z))
-    (.computeLineDistances geom)
-    (let [axis (js/THREE.LineSegments. geom mat js/THREE.LinePieces)]
+    (doseq [[x y z] [[start-x start-y start-z] [end-x end-y end-z]]]
+      (.push (aget geom "vertices") (js/THREE.Vector3. x y z)))
+    ;; (.computeLineDistances geom)
+    (let [axis (line-segments. geom mat js/THREE.LinePieces)]
       axis)))
