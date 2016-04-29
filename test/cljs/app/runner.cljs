4d8adbd2b2052afb20486d81826ea12f33fc1495
Will Medrano <will.s.medrano@gmail.com>
2016-04-29 11:36:29 -0700

    tests

--- test/cljs/app/runner.cljs
+++ test/cljs/app/runner.cljs
@@ -1,7 +1,9 @@
 (ns app.runner
     (:require [doo.runner :refer-macros [doo-tests doo-all-tests]]
+              [view.core-test]
               [utils.core-test]
               [ini-editor.parser-test]))
 
 (doo.runner/doo-tests 'utils.core-test)
 (doo.runner/doo-tests 'ini-editor.parser-test)
+(doo.runner/doo-tests 'view.core-test)
